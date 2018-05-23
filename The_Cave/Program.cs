30aa5f6835a34b37c1773b5afdbd7c3805e1c1a7
Elementalnecro <37450211+Elementalnecro@users.noreply.github.com>
2018-05-23 07:53:03 +0100

    Initial Commit
    Already done some basic prep work for weapons.

--- The_Cave/Program.cs
+++ The_Cave/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace The_Cave
 {
@@ -10,6 +6,100 @@ namespace The_Cave
     {
         static void Main(string[] args)
         {
+            do
+            {
+                PrintIntro();
+                PlayGame();
+                PrintSummary("Ending1");
+
+            } while (AskToPlayAgain());
+
+        }
+
+        public static void PrintIntro()
+        {
+            //Print intro
+
+            //Have some ASCII art
+            Console.WriteLine("       THE CAVE");
+            Console.WriteLine();
+            Console.WriteLine("   ----------------");
+            Console.WriteLine("  /  ------------  \\");
+            Console.WriteLine(" (  (############)  )");
+            Console.WriteLine("(   (############)   )");
+            Console.WriteLine("(   (############)   )");
+            Console.WriteLine("(   (############)   )");
+            Console.WriteLine("----------------------");
+
+            //Tell user what the game is
+            //Explain rules and mechanics to user
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("| -This is a game of adventure!                                              |");
+            Console.WriteLine("| - Choose your profession! Each one gives you different abilities and stats |");
+            Console.WriteLine("| -Find your way out of the cave.                                            |");
+            Console.WriteLine("| -Use items to help you defeat the enemies that stand in your way           |");
+            Console.WriteLine("| -Find treasures that you can take back to the surface!                     |");
+            Console.WriteLine("| -Use words to navigate. Type help for the keywords!                        |");
+            Console.WriteLine("------------------------------------------------------------------------------");
+        }
+
+        public static void PlayGame()
+        {
+            //Run through game
+
+            //Generate Map (Possibly 2d array)
+
+            //Assign objects where necessary
+
+            //Generate items around the area
+            //Fixed areas
+            //Can only hold a certain amount of items (Inventory)
+
+            //Allow the use of objects where necessary
+            //Give items certain boosts
+            //Can drop item if necessary
+
+            //Allow the user to make decision as to where they go
+            //Forward, Back, Left, Right
+
+            //Generate enemies
+            //Generate attack and defense of enemies
+            //Compare enemies stats to user
+            //Depending on stats, depends on damage
+            //DEF ATK HP
+
+            //Have an end goal
+            //Collected the right amount of quest items?
+            //Make it out of the cave?
+            //Kill a certain amount of enemies?
+
+            //FUTURE REF: Perhaps have different quests that the user can choose from for replayability
+            //FUTURE REF: Generate items in random places
+            //FUTURE REF: Generate different maps each time
+
+        }
+
+        public static void PrintSummary(string ending)
+        {
+            //Base summary on choices within the game
+            //2 Good endings
+            //2 Bad endings
+
+            //Show score
+
+        }
+
+        public static bool AskToPlayAgain()
+        {
+            string opt = "";
+
+            Console.WriteLine("Play again if you dare! (Y/N)");
+            opt = Console.ReadLine();
+
+
+            return (opt == "y" || opt == "Y");
         }
     }
 }
---
+++ The_Cave/Weapons.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace The_Cave
+{
+    class Weapons
+    {
+        protected int atkPoints, defPoints = 0;
+
+        public int Attack(int opDef)
+        {
+            if (atkPoints < opDef)
+            {
+                Console.WriteLine("your weapon is not strong enough! No damage done.");
+                return 0;
+            }
+            else if (atkPoints > opDef)
+            {
+                Console.WriteLine("You have wounded your enemy!");
+                return atkPoints - opDef;
+            }
+            else
+            {
+                Console.WriteLine("You are both as strong as eachother.");
+                return 0;
+            }
+        }
+
+        public int Defend(int opAtk, int armourDef)
+        {
+            if ((defPoints + armourDef) < opAtk)
+            {
+                Console.WriteLine("Your defense was broken, you have been hurt!");
+                return opAtk - defPoints;
+            }
+            else if ((defPoints + armourDef) > opAtk)
+            {
+                Console.WriteLine("You have successfully defended!");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("You are both as strong as eachother.");
+                return 0;
+            }
+        }
+
+        public int GetAtk()
+        {
+            return this.atkPoints;
+
+        }
+
+        public int GetDef()
+        {
+            return this.defPoints;
+        }
+    }
+
+    class Dagger : Weapons
+    {
+        public Dagger()
+        {
+            atkPoints = 3;
+            defPoints = 3;
+        }
+    }
+
+    class CrossBow : Weapons
+    {
+        public CrossBow()
+        {
+            atkPoints = 6;
+            defPoints = 1;
+        }
+    }
+
+    class Mace : Weapons
+    {
+        public Mace()
+        {
+            atkPoints = 6;
+            defPoints = 4;
+        }
+    }
+
+    class MorningStar : Weapons
+    {
+        public MorningStar()
+        {
+            atkPoints = 8;
+            defPoints = 2;
+        }
+    }
+
+    class Longsword : Weapons
+    {
+        public Longsword()
+        {
+            atkPoints = 6;
+            defPoints = 3;
+        }
+    }
+
+    class BattleAxe : Weapons
+    {
+        public BattleAxe()
+        {
+            atkPoints = 5;
+            defPoints = 4;
+        }
+    }
+}
